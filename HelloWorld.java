998f8b61ed13475288df5259bd69f39f953dd432
Naresh Parimi <pariminaresh89@gmail.com>
2016-10-09 14:35:19 +0530

    Initial HelloWorld java & Dockerfile

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld{
+
+	public static void main(String args[]){
+          
+          System.out.println("HelloWorld..This is my first Java Application with Dokcer")
+	}
+
+}
